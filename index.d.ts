d10cc43559566993163bc4be078ded8ca25c916a
sutoshunsuke-maker <sutoshunsuke@gmail.com>
2026-08-02 15:48:01 +0900

    Add files via upload

--- index.d.ts
+++ index.d.ts
@@ -1,7 +1,2 @@
-import * as schema from "./schema";
-export declare const pool: import("pg").Pool;
-export declare const db: import("drizzle-orm/node-postgres").NodePgDatabase<typeof schema> & {
-    $client: import("pg").Pool;
-};
-export * from "./schema";
+export {};
 //# sourceMappingURL=index.d.ts.map
